7c655cafab31b47707dfcbdab23194940216c3fc
Vorka Kashek <vorkakashek@bk.ru>
2023-02-07 22:04:17 +0300

    v 1.0.0

---
+++ app/js/app.js
@@ -0,0 +1,218 @@
+'use strict'
+
+// let SpinsAmount = 2,
+//     SpinID = 000001,
+//     PrizeCode = 1,
+//     PrizeName = 'Название Приза',
+//     Promocode = 'promocode2023'
+
+// ПЕРЕМЕННЫЕ: 
+// SpinsAmount - количество оставшихся прокруток (больше их мы не даём делать) integer
+// SpinID - id прокрутки , его возвращаем ajax после того как колесо покрутилось integer 
+// PrizeCode - Код приза , который должен выпасть integer
+// PrizeName - название приза которое можно отображать после прокрутки  string
+// Promocode - промокод на скидку , когда выпадает скидка чтобы его о отразить после прокрутки
+
+// ПРИЗЫ:
+// 0 - Скидка 25% на гайд
+// 1 - KITCHENAID
+// 2 - Деньги 3000 р
+// 3 - Скидка 50% на гайд
+// 4 - Деньги 1000 р
+// 5 - Скидка 15% на гайд
+// 6 - Приз дня!
+// 7 - Деньги 15000 р
+// 8 - VIP тариф Caketeam
+// 9 - Деньги 5000 р
+
+let rotates // Сколько вращений будет сделано (рандомно определяется в функции - от 4 до 9)
+let wheel = document.querySelector('.wheel_inner')
+let counter_value = document.querySelector('.counter_value')
+let btn = document.querySelector('#start')
+let gsap_rotating // здесь будет жить быстрая анимация прокрутки колеса
+
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('[ DOM loaded! ༼ つ ◕_◕ ༽つ ]')
+    wheel_init() // инициализируем колесо / кнопку, из полученных переменных
+})
+
+// POST запрос на сервер
+let request = () => {
+    axios.post('/scrolldone', {
+        'done': SpinID
+    })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+// проверяет существует ли элемент на странице
+let checker = (el) => {
+    if (document.querySelector(el) !== null) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// Инициализация 
+let wheel_init = () => {
+    console.log('Инициализация данных')
+    counter_value.innerHTML = SpinsAmount
+}
+
+// СЧИТАЕМ СКОЛЬКО СПИНОВ ОСТАЛОСЬ
+let spin_counter = () => {
+    SpinsAmount -= 1
+    counter_value.innerHTML = SpinsAmount
+    if (SpinsAmount < 1) {
+        let div = document.querySelector('#counter .counter_inner')
+        div.style.display = 'block'
+        div.innerHTML = 'К сожалению, у вас не осталось попыток. Совершайте покупки на <a href="https://naira-arina.ru" target="_blank">naira-arina.ru</a> и получайте попытки в «Колесе Фортуны»!'
+        btn.classList.add('disabled')
+        btn.style.display = "none"
+    }
+}
+
+// бесконечная анимация прокрутки колеса
+let wheel_rotating = gsap.to(document.querySelector('.wheel_inner'), 60, {
+    rotation: -360,
+    ease: 'linear',
+    repeat: -1,
+})
+
+//появляение венка на фоне
+gsap.from('#wheel_bg img', {
+    rotate: 45,
+    scale: 0.5,
+    ease: 'exp1',
+    duration: .3
+})
+
+// АНИМАЦИЯ ПРОКРУТКИ
+let rotating = () => {
+    gsap_rotating = gsap.timeline()
+        .to('.wheel_inner', {
+            rotate: 0,
+            duration: 0
+        })
+        .to('.wheel_inner', {
+            rotate: rotates,
+            duration: 5,
+            ease: CustomEase.create("custom", "M0,0 C0.11,0.494 0.2,1.01 0.604,1.01 0.672,1.01 0.768,0.989 0.824,0.988 0.868,0.986 0.975,1 1,1 ")
+        })
+        .then(setTimeout(() => { finish() }, 5400))
+}
+
+// ДО ПРОКРУТКИ
+let start = () => {
+    rotates = ((36 * PrizeCode) + (360 * 4)) * -1
+    console.log('start')
+    request()
+    spin_counter()
+
+    wheel_rotating.kill() // убираем бесконечную прокрутку колеса
+    running()
+}
+
+
+// В ПРОЦЕССЕ ПРОКРУТКИ
+let running = () => {
+    console.log('running')
+
+    btn_disable()
+    rotating()
+}
+
+// ПО ЗАВЕРШЕНИИ ПРОКРУТКИ
+let finish = () => {
+    console.log('finish')
+    modal_show()
+    
+    // btn_enable()
+}
+
+let btn_disable = () => {
+    btn.classList.add('disabled')
+}
+
+// let btn_enable = () => {
+//     if (SpinsAmount > 0) {
+//         btn.classList.remove('disabled')
+//     }
+// }
+
+let modal_show = () => {
+    let modal = document.querySelector('#modal')
+    let overlay = document.querySelector('#modal_overlay')
+
+    modal.classList.remove('hide')
+    overlay.classList.remove('hide')
+    
+    modal.querySelector('.modal_prize').innerHTML = PrizeName // подставляем название приза
+    modal.querySelector('.modal_image').src = `./assets/images/dest/prizes/${PrizeCode}.png` // подставляем нужную картинку приза по ID
+
+    if (SpinsAmount > 0) {
+        modal.querySelector('.main_btn').innerHTML = 'Попробовать ещё'
+    } else {
+        modal.querySelector('.main_btn').innerHTML = 'Спасибо'
+    }
+
+    // if (Promocode.length > 0) {
+    //     modal.querySelector('.modal_promocode').classList.remove('hide')
+    //     modal.querySelector('.modal_message').innerHTML = `Воспользуйся этим промокодом на скидку на сайте <a href="https://naira-arina.ru" target="_blank">naira-arina.ru</a>`
+
+    //     modal.querySelector('.modal_promocode').innerHTML = Promocode
+    // } else {
+    //     modal.querySelector('.modal_message').innerHTML = 'Скоро мы с вами свяжемся! :)'
+    //     modal.querySelector('.modal_promocode').classList.add('hide')
+    // }
+}
+
+let modal_hide = () => {
+    let modal = document.querySelector('#modal')
+
+    modal.classList.add('hide')
+    // перезагрузка страницы
+    location.reload()
+}
+
+
+
+// Обрабатываем КЛИК
+document.addEventListener('click', e => {
+    let target = e.target
+
+    if (target.matches('#start')) {
+        start()
+    }
+
+    if (target.matches('.modal_close')) {
+        modal_hide()
+        console.log('asdasd')
+    }
+
+    // Copy to clipboard
+    // if (target.matches('.clipboard')) {
+    //     if (navigator.clipboard) {
+    //         // поддержка имеется, включить соответствующую функцию проекта.
+    //         navigator.clipboard.writeText(target.innerHTML)
+    //             .then(() => {
+    //                 // Получилось!
+    //                 target.classList.add('tooltip')
+    //                 setTimeout(() => {
+    //                     target.classList.remove('tooltip')
+    //                 }, 3000)
+    //             })
+    //             .catch(err => {
+    //                 console.log('Ошибка: ', err);
+    //             });
+    //     } else {
+    //         console.log('Браузер не поддерживает копирование в буфер обмена ^_____^')
+    //         // поддержки нет. Придётся пользоваться execCommand или не включать эту функцию.
+    //     }
+    // }
+})
---
+++ gulpfile.js
@@ -0,0 +1,169 @@
+// Определяем переменную "preprocessor"
+let preprocessor = 'scss';
+
+// Определяем константы Gulp
+const { src, dest, parallel, series, watch } = require('gulp');
+
+// Подключаем Browsersync
+const browserSync = require('browser-sync').create();
+
+// Подключение Pug
+const pug = require('gulp-pug');
+
+// Подключаем gulp-concat
+const concat = require('gulp-concat');
+
+// Подключаем gulp-uglify-es
+const uglify = require('gulp-uglify-es').default;
+
+// Подключаем модули gulp-sass и gulp-less
+const sass = require('gulp-sass')(require('sass'));
+const less = require('gulp-less');
+
+// Подключаем Autoprefixer
+const autoprefixer = require('gulp-autoprefixer');
+
+// Подключаем модуль gulp-clean-css
+const cleancss = require('gulp-clean-css');
+
+// Подключаем compress-images для работы с изображениями
+const imagecomp = require('compress-images');
+
+// Подключаем модуль gulp-clean (вместо del)
+const clean = require('gulp-clean');
+
+// Определяем логику работы Browsersync
+function browsersync() {
+	browserSync.init({ // Инициализация Browsersync
+		server: { baseDir: 'app/' }, // Указываем папку сервера
+		notify: false, // Отключаем уведомления
+		online: true // Режим работы: true или false
+	})
+}
+
+function views() {
+	return src('app/pug/views/*.pug')
+		.pipe(
+			pug({
+				// Your options in here.
+			})
+		)
+		.pipe(dest('app'))
+		.pipe(browserSync.stream()) // Сделаем инъекцию в браузер
+}
+
+function scripts() {
+	return src([ // Берём файлы из источников
+		// 'node_modules/jquery/dist/jquery.min.js', // Пример подключения библиотеки
+		'node_modules/axios/dist/axios.min.js', // Пример подключения библиотеки
+		'node_modules/gsap/dist/gsap.min.js', // Пример подключения библиотеки
+		'node_modules/gsap/dist/CustomEase.min.js', // Пример подключения библиотеки
+		// 'node_modules/swiper/swiper-bundle.min.js', // Пример подключения библиотеки
+		'app/js/app.js', // Пользовательские скрипты, использующие библиотеку, должны быть подключены в конце
+	])
+		.pipe(concat('app.min.js')) // Конкатенируем в один файл
+		.pipe(uglify()) // Сжимаем JavaScript
+		.pipe(dest('app/js/')) // Выгружаем готовый файл в папку назначения
+		.pipe(browserSync.stream()) // Триггерим Browsersync для обновления страницы
+}
+
+function styles() {
+	return src('app/' + preprocessor + '/main.' + preprocessor + '') // Выбираем источник: "app/sass/main.sass" или "app/less/main.less"
+		.pipe(eval(sass)()) // Преобразуем значение переменной "preprocessor" в функцию
+		.pipe(concat('main.min.css')) // Конкатенируем в файл app.min.js
+		.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true })) // Создадим префиксы с помощью Autoprefixer
+		.pipe(cleancss({ level: { 1: { specialComments: 0 } }/* , format: 'beautify' */ })) // Минифицируем стили
+		.pipe(dest('app/css/')) // Выгрузим результат в папку "app/css/"
+		.pipe(browserSync.stream()) // Сделаем инъекцию в браузер
+}
+
+function fonts() {
+	return src('app/assets/fonts/**/*')
+		.pipe(dest('dist/assets/fonts/'))
+		
+}
+
+async function images() {
+	imagecomp(
+		"app/assets/images/src/**/*", // Берём все изображения из папки источника
+		"app/assets/images/dest/", // Выгружаем оптимизированные изображения в папку назначения
+		{ compress_force: false, statistic: true, autoupdate: true }, false, // Настраиваем основные параметры
+		{ jpg: { engine: "mozjpeg", command: ["-quality", "90"] } }, // Сжимаем и оптимизируем изображеня
+		{ png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } }, 
+		{ svg: { engine: "svgo", command: "--multipass" } },
+		{ gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+		function (err, completed) { // Обновляем страницу по завершению
+			if (completed === true) {
+				browserSync.reload()
+			}
+		}
+	)
+}
+
+function cleanimg() {
+	return src('app/assets/images/dest/', { allowEmpty: true }).pipe(clean()) // Удаляем всё содержимое папки "app/images/dest/"
+}
+
+function buildcopy() {
+	return src([ // Выбираем нужные файлы
+		'app/css/**/*.min.css',
+		'app/js/**/*.min.js',
+		'app/assets/images/dest/**/*',
+		'app/**/*.html',
+	], { base: 'app' }) // Параметр "base" сохраняет структуру проекта при копировании
+		.pipe(dest('dist')) // Выгружаем в папку с финальной сборкой
+}
+
+function cleandist() {
+	return src('dist', { allowEmpty: true }).pipe(clean()) // Удаляем всё содержимое папки "dist/"
+}
+
+function startwatch() {
+
+	// Выбираем все файлы JS в проекте, а затем исключим с суффиксом .min.js
+	watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
+
+	// Мониторим файлы препроцессора на изменения
+	watch('app/**/' + preprocessor + '/**/*', styles);
+
+	// Мониторим файлы PUG на изменения
+	watch('app/**/*.pug').on('change', views);
+
+	// Мониторим файлы HTML на изменения
+	watch('app/**/*.html').on('change', browserSync.reload);
+
+	// Мониторим папку-источник изображений и выполняем images(), если есть изменения
+	watch('app/assets/images/src/**/*', images);
+
+	// Мониторим папку-источник шрифтов и выполняем fonts(), если есть изменения
+	watch('app/assets/fonts/**/*', fonts)
+
+}
+
+
+// Экспортируем функцию browsersync() как таск browsersync. Значение после знака = это имеющаяся функция.
+exports.browsersync = browsersync;
+
+// Экспорт функции views()  в таск views
+exports.views = views;
+
+// Экспортируем функцию scripts() в таск scripts
+exports.scripts = scripts;
+
+// Экспортируем функцию styles() в таск styles
+exports.styles = styles;
+
+// Экспорт функции images() в таск images
+exports.images = images;
+
+// Экспорт функции fonts() в таск fonts
+exports.fonts = fonts;
+
+// Экспортируем функцию cleanimg() как таск cleanimg
+exports.cleanimg = cleanimg;
+
+// Создаём новый таск "build", который последовательно выполняет нужные операции
+exports.build = series(cleandist, styles, scripts, images, buildcopy, views, fonts);
+
+// Экспортируем дефолтный таск с нужным набором функций
+exports.default = parallel(styles, scripts, browsersync, startwatch, views, fonts);
